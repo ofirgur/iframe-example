b96c9cd823d885245e452963e1cc4c1c068357e9
Ofir <ofirgu@payoneer.com>
2020-04-27 16:31:47 +0300

    update params

--- js/index.js
+++ js/index.js
@@ -2,6 +2,10 @@ const sendMassageToParent = () => {
     window.top.postMessage({
         message: 'payoneer-plugin',
         type: 'deeplink',
-        pathname: '/?action=withdrawal'
+        pathname: '/?action=withdrawal',
+        params: {
+            p1: 'p1',
+            p2: 'p2'
+        }
     }, '*');
 };
