df943e3fab0141a666f820ed4a73952126923a34
Ofir <ofirgu@payoneer.com>
2020-04-28 11:22:47 +0300

    update ready

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
+(() => {
+    setTimeout(() => {
+        sendMassageToParent('ready');
+    }, 5000);
+})();
+
 const sendMassageToParent = type => {
     window.top.postMessage({
         message: 'payoneer-plugin',
         type,
         params: type === 'deeplink' ? {
             pathname: '/?action=withdrawal'
-        } : {
-            
-        }
+        } : {}
     }, '*');
 };
