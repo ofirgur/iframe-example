1c43bfa6096dacdb0bd290b266647d1c0ae552b5
ofirgur <ofirgur@yahoo.com>
2020-12-28 17:30:49 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 (() => {
-    console.log('Hello world!');
     setTimeout(() => {
         sendMassageToParent('ready');
     }, 5000);
 })();
 
 const sendMassageToParent = type => {
+    console.log('sendMassageToParent: ', type);
+    
     const params = {
         pathname: type === 'deeplink' && 'http://localhost:8080/?action=withdrawal',
         successMsg: type === 'toast' && 'Success toast!!!'
